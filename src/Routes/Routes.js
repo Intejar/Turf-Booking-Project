5ba5589d77c590c57014819cb5c0b64ea9622738
Intejar <meherabintejar@gmail.com>
2023-11-19 01:22:36 +0600

    Project Setup Done

---
+++ src/Routes/Routes.js
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+import DashboardLayout from "../../Layout/Dashboard";
+import UserProfile from "../../Dashboard/UserProfile/UserProfile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+      {
+        path: "/dashboard",
+        element: <UserProfile></UserProfile>,
+      },
+    ],
+  },
+  // {
+  //     path:'*',
+  //     element:<NotFound></NotFound>
+  // }
+]);
+
+export default router;
