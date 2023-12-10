5e5518c9e371d6ee4b1aa45eeea262c1003a954d
Intejar <meherabintejar@gmail.com>
2023-12-10 14:35:29 +0600

    home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TurfCard from "./TurfCard";
+import ShopCard from "./ShopCard";
 
 const Home = () => {
-  return <div className="text-2xl font-bold">This is Home page</div>;
+  const [topTurf, setTopTurf] = useState([]);
+  const [topSell, setTopSell] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/turfCollection`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("top", data);
+        setTopTurf(data);
+      });
+  }, []);
+  useEffect(() => {
+    fetch(`http://localhost:5000/shop`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("top", data);
+        setTopSell(data);
+      });
+  }, []);
+  const topTurfData = topTurf.filter((item) => item.advertise === "True");
+  const shopData = topSell.filter((item) => item.advertise === "True");
+
+  return (
+    <div className="mx-10">
+      <div className="text-3xl font-bold text-green-800">Top Turf</div>
+      <div className="w-20 h-2 rounded-sm bg-green-600"></div>
+      <div className="grid  grid-cols-3  ml-20 my-5">
+        {topTurfData.map((b) => (
+          <TurfCard key={b._id} turfInfo={b}></TurfCard>
+        ))}
+      </div>
+      <div className="text-3xl font-bold text-green-800">Top Selling</div>
+      <div className="w-20 h-2 rounded-sm bg-green-600"></div>
+      <div className="grid  grid-cols-3  ml-20 my-5">
+        {shopData.map((b) => (
+          <ShopCard key={b._id} productInfo={b}></ShopCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
